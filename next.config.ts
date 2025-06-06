2d6428101de3e9957858bf17ba5455bb16a465c1
MicheleMeloni29 <melonimichelee29@gmail.com>
2025-06-06 14:27:55 +0200

    Deploy personal website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Serve per creare un sito statico
+  distDir: 'out',   // Dove verrà generato il sito statico
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // Necessario per GitHub Pages
+  basePath: '/personalWebsite', // <--- cambia con il nome della repo
 };
 
-export default nextConfig;
+module.exports = nextConfig;
